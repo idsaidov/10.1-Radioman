d090040cfe2dc1674562391bdb593da9da424770
idsaidov <id.saidov@yandex.ru>
2022-09-02 16:31:59 +0300

    Updated the java class

--- src/main/java/Radio.java
+++ src/main/java/Radio.java
@@ -1,2 +1,64 @@
 public class Radio {
+    private int currentStation;
+    private int currentVolume;
+    public int minVolume = 0;
+    public int maxVolume = 10;
+    public int firstStation = 0;
+    public int lastStation = 9;
+
+    public void nextStation() {
+        if (currentStation < lastStation) {
+            currentStation++;
+        } else {
+            currentStation = firstStation;
+        }
+    }
+
+    public void prevStation() {
+        if (currentStation > firstStation) {
+            currentStation--;
+        } else {
+            currentStation = lastStation;
+        }
+    }
+
+    public int getCurrentStation() {
+        return currentStation;
+    }
+
+    public void setCurrentStation(int currentStation) {
+        if (currentStation < firstStation) {
+            return;
+        }
+        if (currentStation > lastStation) {
+            return;
+        }
+        this.currentStation = currentStation;
+    }
+
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
+
+    public void setCurrentVolume(int currentVolume) {
+        if (currentVolume < minVolume) {
+            return;
+        }
+        if (currentVolume > maxVolume) {
+            return;
+        }
+        this.currentVolume = currentVolume;
+    }
+
+    public void increaseTheSoundVolumeLevel() {
+        if (currentVolume < maxVolume) {
+            currentVolume++;
+        }
+    }
+
+    public void reduceTheSoundVolumeLevel() {
+        if (currentVolume > minVolume) {
+            currentVolume--;
+        }
+    }
 }
--- src/test/java/RadioTest.java
+++ src/test/java/RadioTest.java
@@ -1,2 +1,64 @@
-public class RadioTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+class RadioTest {
+    Radio radio = new Radio();
+
+    @ParameterizedTest()
+    @CsvFileSource(files="src/test/resources/nextStation.csv")
+    void shouldSetNextStation(String name, int station, int expected) {
+        radio.setCurrentStation(station);
+        radio.nextStation();
+        int actual = radio.getCurrentStation();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest()
+    @CsvFileSource(files="src/test/resources/prevStation.csv")
+    void shouldSetPrevStation(String name, int station, int expected) {
+        radio.setCurrentStation(station);
+        radio.prevStation();
+        int actual = radio.getCurrentStation();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest()
+    @CsvFileSource(files="src/test/resources/setStation.csv")
+    void shouldSetStation(String name, int station, int expected) {
+        radio.setCurrentStation(station);
+        int actual = radio.getCurrentStation();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest()
+    @CsvFileSource(files="src/test/resources/increaseVolume.csv")
+    void shouldIncreaseVolume(String name, int volume, int expected) {
+        radio.setCurrentVolume(volume);
+        radio.increaseTheSoundVolumeLevel();
+        int actual = radio.getCurrentVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest()
+    @CsvFileSource(files="src/test/resources/decreaseVolume.csv")
+    void shouldDecreaseVolume(String name, int volume, int expected) {
+        radio.setCurrentVolume(volume);
+        radio.reduceTheSoundVolumeLevel();
+        int actual = radio.getCurrentVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest()
+    @CsvFileSource(files="src/test/resources/setCurrentVolume.csv")
+    void shouldSetCurrentVolume(String name, int volume, int expected) {
+        radio.setCurrentVolume(volume);
+        int actual = radio.getCurrentVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
 }
